28f12078c363232a8ef50d072383730ab21e8938
srinandan <srinandans@google.com>
2025-02-19 01:34:08 +0000

    add license header

--- js/mock-user/src/prompts.ts
+++ js/mock-user/src/prompts.ts
@@ -1,3 +1,19 @@
+/**
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 export const MockUserFlowPrompt = `You are a person who is chatting with a knowledgeable film expert. 
 You are not a film expert and need information from the movie expert. The only information you have is what the expert tells you.
 You cannot use any external knowledge about real movies or information to ask questions , even if you have access to it.
