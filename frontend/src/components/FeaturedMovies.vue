307b4b9bd4b3a95657ee43b7c2875d9c2a71cb66
srinandan <srinandans@google.com>
2025-02-22 16:32:36 +0000

    adds missing featured movies

--- frontend/src/components/FeaturedMovies.vue
+++ frontend/src/components/FeaturedMovies.vue
@@ -1,6 +1,10 @@
 <template>
-      <MovieCard :movies="store.getters['chat/placeHolderMovies']"></MovieCard>
-  </template>
+  <h1 class="font-guru-title text-xl font-bold uppercase tracking-wide text-[#C6D4D2] 
+drop-shadow-2xl text-center mb-5">
+    Featured films
+  </h1>
+  <MovieCard :movies="store.getters['chat/placeHolderMovies']"></MovieCard>
+</template>
   
   <script>
   import store  from '../stores';
